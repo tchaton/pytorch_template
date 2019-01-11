5841247e5e47836f435baba601f42cd1217c4379
Your Name <you@example.com>
2019-01-11 15:36:46 +0000

    'Minimal pytorch project v0.1'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='project_name',
+      version='0.1',
+      description='Minimal Pytorch Project',
+      url='http://github.com/storborg/funniest',
+      author='Thomas Chaton',
+      author_email='thomas.chaton.ai@gmail.com',
+      license='MIT',
+      packages=['project_name'],
+      zip_safe=False)
--- train.py
+++ train.py
@@ -19,10 +19,12 @@ See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-p
 See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
 """
 import time
-from options.train_options import TrainOptions
-from data import create_dataset
-from models import create_model
-from util.visualizer import Visualizer
+
+from project_name.models import create_model
+from project_name.options.train_options import TrainOptions
+
+from project_name.data import create_dataset
+from project_name.util.visualizer import Visualizer
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
